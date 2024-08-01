4eb7f2fefb74d9ca7a9ff0436f736360eeff3cee
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-01 11:11:25 -0500

    Se crea componente de login (Iniciar Sesión)

--- ECommerceWeb/src/app/app.module.ts
+++ ECommerceWeb/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemoAngularMaterialModuleTsComponent } from './DemoAngularMaterialModule';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
