a5a1afeafc6fbb9cf93546e63369ab12534cedd4
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-01 11:23:19 -0500

    Enrutando los componentes que creamos. Cuando se crean módulos si se enruta de forma automatica.

--- ECommerceWeb/src/app/app-routing.module.ts
+++ ECommerceWeb/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [{ path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }, { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) }, { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
+const routes: Routes = [
+  // Añadiendo los dos componentes que creamos (login, signup)
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'customer', loadChildren: () => import('./customer/customer.module').then(m => m.CustomerModule) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
