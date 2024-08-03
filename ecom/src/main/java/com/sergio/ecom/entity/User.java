a5cb98b6530ff2fed672316bc2c458340eac7be6
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-03 09:15:11 -0500

    Se actualizo el plugin y ahora funciona a la perfección Lombook.

--- ecom/src/main/java/com/sergio/ecom/entity/User.java
+++ ecom/src/main/java/com/sergio/ecom/entity/User.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-//@Data - No me estaba funcionando para obtener información en otra clase.
+@Data //- Ya funciona tenía que actualizar el plugin.
 @Table(name = "users")
 public class User {
 
@@ -23,64 +23,5 @@ public class User {
     @Column(columnDefinition = "longblob")
     private byte[] img;
 
-    public User() {
-    }
 
-    public User(Long id, String email, String password, String name, UserRole role, byte[] img) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.name = name;
-        this.role = role;
-        this.img = img;
-    }
-
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
-    public byte[] getImg() {
-        return img;
-    }
-
-    public void setImg(byte[] img) {
-        this.img = img;
-    }
 }
