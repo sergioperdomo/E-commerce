42bcc8402bf1c6e262343fa0b5aaa2bf779c9fbc
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-03 17:23:49 -0500

    Se crea los atributos, los constructores (vacios y con parametros), setters y getters, gracias a Lombook y esto se creo dentro de la carpeta del DTO.

---
+++ ecom/src/main/java/com/sergio/ecom/dto/SignupRequest.java
@@ -0,0 +1,13 @@
+package com.sergio.ecom.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+
+    private String email;
+    private String password;
+    private String name;
+
+
+}
