5a194546102a7f77164e8eebf4d1e67ecd94be52
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-04 20:31:00 -0500

    Añadiendo el método de encontrar por el rol.

--- ecom/src/main/java/com/sergio/ecom/repository/UserRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sergio.ecom.repository;
 
 import com.sergio.ecom.entity.User;
+import com.sergio.ecom.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ public interface UserRepository  extends JpaRepository<User, Long> {
 
     Optional<User> findFirstByEmail(String email);
 
+    User findByRole(UserRole userRole);
 
 
 }
