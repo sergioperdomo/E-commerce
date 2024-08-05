497933187ba4e2cb47ecb2a46e083dac8dc3ca27
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-04 22:34:14 -0500

    Creación del Servicio.

---
+++ ECommerceWeb/src/app/services/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+const BASIC_URL = "http://localhost:8080/"; // URL de nuestra aplicación BackEnd.
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
