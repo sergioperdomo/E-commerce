44234d0b7b6662cab8bbecaab0b0109d0d144a5e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-05 11:25:57 -0500

    Métodos de Guarda el token y el usuario.

--- ECommerceWeb/src/app/services/storage/user-storage.service.ts
+++ ECommerceWeb/src/app/services/storage/user-storage.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
 
+const TOKEN = 'ecom-token';
+const USER = 'ecom.user';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UserStorageService {
 
   constructor() { }
+
+
+
+  public saveToken( token: string ): void {
+    // Eliminamos el token que se encuentra almacenado localmente.
+
+    window.localStorage.removeItem(TOKEN);
+
+    // Almacenamos un nuevo token.
+
+    window.localStorage.setItem(TOKEN, token);
+  }
+
+
+  public saveUser( user ): void {
+    // Eliminamos el usuario que se encuentra almacenado localmente.
+
+    window.localStorage.removeItem(USER);
+
+    // Almacenamos un nuevo usuario.
+
+    window.localStorage.setItem(TOKEN, JSON.stringify(user));
+  }
+
+
+
+
 }
