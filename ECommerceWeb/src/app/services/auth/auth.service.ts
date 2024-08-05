1c07d6f514cbaa065c5d73728c8a87c26ecfb3c7
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-05 10:23:24 -0500

    Servicio de registro

--- ECommerceWeb/src/app/services/auth/auth.service.ts
+++ ECommerceWeb/src/app/services/auth/auth.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 const BASIC_URL = "http://localhost:8080/"; // URL de nuestra aplicación BackEnd.
 
@@ -7,5 +9,11 @@ const BASIC_URL = "http://localhost:8080/"; // URL de nuestra aplicación BackEn
 })
 export class AuthService {
 
-  constructor() { }
+  constructor( private http: HttpClient ) { }
+
+  register(sigunupRequest: any): Observable<any>{
+    return this.http.post(BASIC_URL + "sign-up", sigunupRequest);
+  }
+
+
 }
