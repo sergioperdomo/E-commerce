4322b9142d35457f7cb7597cbb8b4638df2a1fbf
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-05 12:34:34 -0500

    Validamos los campos del formulario.

--- ECommerceWeb/src/app/login/login.component.ts
+++ ECommerceWeb/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth/auth.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +10,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+  loginForm!: FormGroup;
+  hidePassword = true;
+
+  constructor (
+    private FormBuilder: FormBuilder,
+    private authService: AuthService,
+    private snackbar: MatSnackBar,
+    private router: Router,
+
+  ){  }
+
+  ngOnInit() : void {
+    this.loginForm = this.FormBuilder.group({
+      email : [null, [Validators.required]],
+      password : [null, [Validators.required]]
+    })
+  }
+
+  togglePasswordVisibility(){
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit(): void{
+
+    const username = this.loginForm.get('email')!.value;
+    const password = this.loginForm.get('pssword')!.value;
+
+    this.authService.login(username, password).subscribe(
+      (res) => {
+        this.snackbar.open('Login Success', 'Ok', { duration : 5000 })
+      },
+      (error) => {
+        this.snackbar.open('Bad Credentials', 'ERROR', { duration : 5000 })
+      }
+    );
+
+  }
+
+
+
 
 }
