0717948f38cdbfeac342a096f9e0b2c6131e1229
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-07 11:45:57 -0500

    Creando métodos.

--- ECommerceWeb/src/app/services/storage/user-storage.service.ts
+++ ECommerceWeb/src/app/services/storage/user-storage.service.ts
@@ -33,7 +33,53 @@ export class UserStorageService {
     window.localStorage.setItem(USER, JSON.stringify(user));
   }
 
+  // Este método obtendra el token de nuestro almacenamiento en local y lo retornara en una cadena.
+  static getToken(): string {
+    return localStorage.getItem(TOKEN);
+  }
 
+  static getUser(): any {
+    return JSON.parse(localStorage.getItem(USER));
+  }
 
+  static getUserId(): string {
+    const user = this.getUser();
+    if(user == null ) {
+      return '';
+    }
+    return user.userId;
+  }
+
+  static getUserRole(): string {
+    const user = this.getUser();
+    if(user == null ) {
+      return '';
+    }
+    return user.role;
+  }
+
+  // Método que nos permite verificar si el usuario o persona que inicio sesión es administrdaor o no.
+  static isAdminLoggedIn(): boolean {
+    if (this.getToken === null){
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role == 'ADMIN';
+  }
+
+   // Método que nos permite verificar si el usuario o persona que inicio sesión es cliente o no.
+   static isCustomerLoggedIn(): boolean {
+    if (this.getToken === null){
+      return false;
+    }
+    const role: string = this.getUserRole();
+    return role == 'CUSTOMER';
+  }
+
+  // Método de cerrar sesión.
+  static signOut(): void {
+    window.localStorage.removeItem(TOKEN);
+    window.localStorage.removeItem(USER);
+  }
 
 }
