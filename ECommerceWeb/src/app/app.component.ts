3799541756053c82ceff2577cecab3f7a5a02896
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-07 11:47:26 -0500

    Creando dos variables  para comprobar si el usuario que ha iniciado sesión es cliente o administrador.

--- ECommerceWeb/src/app/app.component.ts
+++ ECommerceWeb/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { UserStorageService } from './services/storage/user-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ECommerceWeb';
+
+  // Creando dos variables  para comprobar si el usuario que ha iniciado sesión es cliente o administrador.
+  isCustomerLoggedIn : boolean = UserStorageService.isCustomerLoggedIn();
+  isAdminLoggedIn : boolean = UserStorageService.isAdminLoggedIn();
+
+  constructor(private router : Router){ }
+
+  ngOnInit(): void {
+
+    this.router.events.subscribe(event => {
+      this.isCustomerLoggedIn = UserStorageService.isCustomerLoggedIn();
+      this.isAdminLoggedIn = UserStorageService.isAdminLoggedIn();
+    })
+
+  }
+
+  // Método para cerrar sesión
+  logout() {
+    UserStorageService.signOut();
+    this.router.navigateByUrl('login');
+  }
+
 }
