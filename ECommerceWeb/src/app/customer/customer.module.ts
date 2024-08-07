196038a0d8e32363cd0d3a012d3421d94d7ee5e1
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-07 12:12:38 -0500

    Se enruta de forma automatica cuando se crea el componente.

--- ECommerceWeb/src/app/customer/customer.module.ts
+++ ECommerceWeb/src/app/customer/customer.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgModule } from '@angular/core';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
-    DashboardComponent
+    DashboardCustomerComponent
   ],
   imports: [
     CommonModule,
