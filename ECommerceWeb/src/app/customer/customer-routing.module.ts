8907d650e8c3f07305577e6ce4947cb98294685b
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-07 12:15:30 -0500

    Enrutando nuestro compontente DashboardCustomerComponent

--- ECommerceWeb/src/app/customer/customer-routing.module.ts
+++ ECommerceWeb/src/app/customer/customer-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CustomerComponent } from './customer.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
 
 
 const routes: Routes = [
   { path: '', component: CustomerComponent },
-  { path: 'dashboard', component: DashboardComponent }
+  { path: 'dashboard', component: DashboardCustomerComponent }
 ];
 
 @NgModule({
