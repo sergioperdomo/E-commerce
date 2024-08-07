37fef30340700c9069363fc8a56ed7f66f3da573
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-07 17:51:39 -0500

    Creando la API, básicamente crea la categoria en el cuerpo de la petición, en caso de crearse correctamente enviara una respuesta de CREATED al cuerpo de la petición.

---
+++ ecom/src/main/java/com/sergio/ecom/controller/admin/AdminCategoryController.java
@@ -0,0 +1,29 @@
+package com.sergio.ecom.controller.admin;
+
+
+import com.sergio.ecom.dto.CategoryDto;
+import com.sergio.ecom.entity.Category;
+import com.sergio.ecom.services.admin.category.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("category")
+    public ResponseEntity<Category> createCategory (@RequestBody CategoryDto categoryDto){
+        Category category = categoryService.createCategory(categoryDto);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+}
