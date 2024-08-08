bc65c42c38dc12c5ce6c8efb7d594aab8373d07b
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-08 11:14:52 -0500

    Creando método de agregar una categoria y otro método para autorizar.

--- ECommerceWeb/src/app/admin/service/admin.service.ts
+++ ECommerceWeb/src/app/admin/service/admin.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
 
 // URL de nuestra aplicación BackEnd
 
@@ -11,4 +13,19 @@ const BASIC_URL = "http://localhost:8080/";
 export class AdminService {
 
   constructor( private http: HttpClient) { }
+
+  // Método para agregar una categoria.
+
+  addCategory(categoryDto: any): Observable<any>{
+    return this.http.post(BASIC_URL + 'api/admin/category', categoryDto, {
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+  //  Método para autorizar
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer ' + UserStorageService.getToken()
+    )
+  }
+
 }
