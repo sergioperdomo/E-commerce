3452bb8dee6f9601c88f455f559f451fab7a36a7
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-08 11:03:54 -0500

    Importando:     FormsModule,     ReactiveFormsModule,     HttpClientModule,     DemoAngularMaterialModuleTsComponent

--- ECommerceWeb/src/app/admin/admin.module.ts
+++ ECommerceWeb/src/app/admin/admin.module.ts
@@ -4,6 +4,9 @@ import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
 import { DashboradAdminComponent } from './components/dashborad-admin/dashborad-admin.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DemoAngularMaterialModuleTsComponent } from '../DemoAngularMaterialModule';
 // import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
@@ -15,7 +18,11 @@ import { DashboradAdminComponent } from './components/dashborad-admin/dashborad-
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DemoAngularMaterialModuleTsComponent
   ]
 })
 export class AdminModule { }
