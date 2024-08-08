397024d4dc2be37e18365fc2096e003e839fa5fc
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-08 11:18:36 -0500

    Dandole una ruta al componente que se acabo de crear.

--- ECommerceWeb/src/app/admin/admin-routing.module.ts
+++ ECommerceWeb/src/app/admin/admin-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { DashboradAdminComponent } from './components/dashborad-admin/dashborad-admin.component';
+import { PostCategoryComponent } from './components/post-category/post-category.component';
 
 
 
 const routes: Routes = [
   { path: '', component: AdminComponent },
-  { path: 'dashboard', component: DashboradAdminComponent }
+  { path: 'dashboard', component: DashboradAdminComponent },
+  { path: 'category', component: PostCategoryComponent }
 ];
 
 @NgModule({
