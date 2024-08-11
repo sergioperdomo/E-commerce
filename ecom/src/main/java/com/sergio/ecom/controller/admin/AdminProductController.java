1de1c7c7b7f172307093b17fd64f8f53838eea46
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 16:17:19 -0500

    Agregando y obteniendo productos.

--- ecom/src/main/java/com/sergio/ecom/controller/admin/AdminProductController.java
+++ ecom/src/main/java/com/sergio/ecom/controller/admin/AdminProductController.java
@@ -1,2 +1,33 @@
-package com.sergio.ecom.controller.admin;public class AdminProductController {
+package com.sergio.ecom.controller.admin;
+
+import com.sergio.ecom.dto.ProductDto;
+import com.sergio.ecom.services.admin.adminproduct.AdminProductService;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminProductController {
+
+    private final AdminProductService adminProductService;
+
+    @PostMapping("/product")
+    public ResponseEntity<ProductDto> addProduct (@ModelAttribute ProductDto productDto) throws IOException {
+        ProductDto productDto1 = adminProductService.addProduct(productDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productDto1);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDto>> getAllProducts () {
+        List<ProductDto> productDtos = adminProductService.getAllProducts();
+        return ResponseEntity.ok(productDtos);
+    }
+
 }
