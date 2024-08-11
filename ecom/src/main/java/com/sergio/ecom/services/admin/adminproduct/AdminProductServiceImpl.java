86f5eb4f949b98f77e0537b5d7ef75a7bc45aa56
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 16:15:09 -0500

    Métodos implementados.

--- ecom/src/main/java/com/sergio/ecom/services/admin/adminproduct/AdminProductServiceImpl.java
+++ ecom/src/main/java/com/sergio/ecom/services/admin/adminproduct/AdminProductServiceImpl.java
@@ -1,2 +1,41 @@
-package com.sergio.ecom.services.admin.adminproduct;public class AdminProductServiceImpl {
+package com.sergio.ecom.services.admin.adminproduct;
+
+import com.sergio.ecom.dto.ProductDto;
+import com.sergio.ecom.entity.Category;
+import com.sergio.ecom.entity.Product;
+import com.sergio.ecom.repository.CategoryRepository;
+import com.sergio.ecom.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AdminProductServiceImpl implements AdminProductService {
+
+    private final ProductRepository productRepository;
+
+    private final CategoryRepository categoryRepository;
+
+    public ProductDto addProduct (ProductDto productDto) throws IOException {
+        Product product = new Product();
+        product.setName(productDto.getName());
+        product.setDescription(productDto.getDescription());
+        product.setPrice(productDto.getPrice());
+        product.setImg(productDto.getImg().getBytes());
+
+        Category category = categoryRepository.findById(productDto.getCategoryId()).orElseThrow();
+
+        product.setCategory(category);
+        return productRepository.save(product).getDto();
+    }
+
+    public List<ProductDto> getAllProducts () {
+        List<Product> products = productRepository.findAll();
+        return products.stream().map(Product::getDto).collect(Collectors.toList());
+    }
+
 }
