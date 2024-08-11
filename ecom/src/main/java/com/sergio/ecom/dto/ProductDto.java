19d421fce159229ac6708f4b7c261a5fc2a8185e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 15:52:07 -0500

    Dto del producto.

--- ecom/src/main/java/com/sergio/ecom/dto/ProductDto.java
+++ ecom/src/main/java/com/sergio/ecom/dto/ProductDto.java
@@ -1,2 +1,24 @@
-package com.sergio.ecom.dto;public class ProductDto {
+package com.sergio.ecom.dto;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class ProductDto {
+
+    private Long id;
+
+    private String name;
+    private Long price;
+
+
+    private String description;
+
+    private byte[] byteImg;
+
+    private Long categoryId;
+
+    private MultipartFile img;
+
 }
