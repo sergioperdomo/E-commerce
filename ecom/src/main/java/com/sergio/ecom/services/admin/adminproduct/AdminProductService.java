c390329d970478411b11f82a076fd34d7b9d8e79
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 16:14:50 -0500

    Nombrando métodos que se van a usar.

--- ecom/src/main/java/com/sergio/ecom/services/admin/adminproduct/AdminProductService.java
+++ ecom/src/main/java/com/sergio/ecom/services/admin/adminproduct/AdminProductService.java
@@ -1,2 +1,14 @@
-package com.sergio.ecom.services.admin.adminproduct;public class AdminProductService {
+package com.sergio.ecom.services.admin.adminproduct;
+
+import com.sergio.ecom.dto.ProductDto;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface AdminProductService {
+
+    ProductDto addProduct (ProductDto productDto) throws IOException;
+
+    List<ProductDto> getAllProducts ();
+
 }
