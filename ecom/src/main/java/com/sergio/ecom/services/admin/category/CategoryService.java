cc7da71a7da808969c6e8d6e90852e906eefb877
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 16:15:54 -0500

    Métodos de servicio que se van ha implementar.

--- ecom/src/main/java/com/sergio/ecom/services/admin/category/CategoryService.java
+++ ecom/src/main/java/com/sergio/ecom/services/admin/category/CategoryService.java
@@ -3,9 +3,13 @@ package com.sergio.ecom.services.admin.category;
 import com.sergio.ecom.dto.CategoryDto;
 import com.sergio.ecom.entity.Category;
 
+import java.util.List;
+
 public interface CategoryService {
 
     // Método para crear una categoria
     Category createCategory (CategoryDto categoryDto);
+
+    List<Category> getAllCategories();
 
 }
