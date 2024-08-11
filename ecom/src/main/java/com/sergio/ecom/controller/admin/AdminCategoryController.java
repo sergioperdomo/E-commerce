150f17f430bce60f187ac68764215e2df9abe420
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-11 16:16:17 -0500

    Métodos para obtener todas las categoria.

--- ecom/src/main/java/com/sergio/ecom/controller/admin/AdminCategoryController.java
+++ ecom/src/main/java/com/sergio/ecom/controller/admin/AdminCategoryController.java
@@ -7,10 +7,9 @@ import com.sergio.ecom.services.admin.category.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -24,6 +23,11 @@ public class AdminCategoryController {
         Category category = categoryService.createCategory(categoryDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<Category>> getAllCategories (){
+        return ResponseEntity.ok(categoryService.getAllCategories());
     }
 
 }
