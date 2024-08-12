e3dea7c89553731f6141c33122df42549e443880
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-12 16:32:57 -0500

    Obteniendo todos los productos junto con la imagen.

--- ECommerceWeb/src/app/admin/components/dashborad-admin/dashborad-admin.component.ts
+++ ECommerceWeb/src/app/admin/components/dashborad-admin/dashborad-admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AdminService } from '../../service/admin.service';
 
 @Component({
   selector: 'app-dashborad-admin',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashborad-admin.component.scss']
 })
 export class DashboradAdminComponent {
+
+  products: any[] = [];
+
+
+  constructor ( private adminService: AdminService ) { }
+
+  ngOnInit() {
+    this.getAllProducts();
+  }
+
+  getAllProducts() {
+    this.products = [];
+    this.adminService.getAllProducts().subscribe(res => {
+      res.forEach(element => {
+        element.processedImg = 'data:image/jpeg;bases64' + element.byteImg;
+        this.products.push(element);
+      });
+    })
+  }
 
 }
