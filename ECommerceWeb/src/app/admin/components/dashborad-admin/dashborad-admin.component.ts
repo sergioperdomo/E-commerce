35648c939240c51616266f05d2c8aa32ae48b965
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-16 11:53:55 -0500

    Boton de enviar.

--- ECommerceWeb/src/app/admin/components/dashborad-admin/dashborad-admin.component.ts
+++ ECommerceWeb/src/app/admin/components/dashborad-admin/dashborad-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-dashborad-admin',
   templateUrl: './dashborad-admin.component.html',
@@ -8,17 +9,34 @@ import { AdminService } from '../../service/admin.service';
 export class DashboradAdminComponent {
 
   products: any[] = [];
+  searchProductForm!: FormGroup;
 
 
-  constructor ( private adminService: AdminService ) { }
+  constructor ( private adminService: AdminService, private fb: FormBuilder ) { }
 
   ngOnInit() {
     this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: [null, [Validators.required]]
+    })
   }
 
   getAllProducts() {
     this.products = [];
     this.adminService.getAllProducts().subscribe(res => {
+      // console.log('getAllProducts response:', res);
+      res.forEach(element => {
+        element.processedImg = 'data:image/png;base64,'+ element.byteImg;
+        this.products.push(element);
+      });
+    })
+  }
+
+  // Envio del formulario
+  submitForm() {
+    this.products = [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.adminService.getAllProductsByName(title).subscribe(res => {
       // console.log('getAllProducts response:', res);
       res.forEach(element => {
         element.processedImg = 'data:image/png;base64,'+ element.byteImg;
