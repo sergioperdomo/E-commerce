c1f068c73d5e1cfd7cc87f8030aeb2bda561f7c8
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-16 10:45:46 -0500

    Método para obtener todos los productos que cuente o tengan el mismo nombre independientemente de la posición que se encuentre el producto.

--- ecom/src/main/java/com/sergio/ecom/repository/ProductRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/ProductRepository.java
@@ -5,6 +5,12 @@ import com.sergio.ecom.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository  extends JpaRepository<Product,Long> {
+
+    // Este método nos permite encontrar el producto por el nombre
+    List<Product> findAllByNameContaining(String title);
+
 }
