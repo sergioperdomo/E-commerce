b0e10bd02113e0d96236332b8ac723affbf3c646
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-17 16:55:11 -0500

    Importando la diferentes modulos.

--- ECommerceWeb/src/app/customer/customer.module.ts
+++ ECommerceWeb/src/app/customer/customer.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { CustomerRoutingModule } from './customer-routing.module';
 import { CustomerComponent } from './customer.component';
 import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { DemoAngularMaterialModuleTsComponent } from '../DemoAngularMaterialModule';
 
 
 @NgModule({
@@ -12,7 +15,12 @@ import { DashboardCustomerComponent } from './components/dashboard-customer/dash
   ],
   imports: [
     CommonModule,
-    CustomerRoutingModule
+    CustomerRoutingModule,
+    DemoAngularMaterialModuleTsComponent,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
+
   ]
 })
 export class CustomerModule { }
