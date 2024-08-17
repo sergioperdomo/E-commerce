662c457657f8a4c2e7709fb222008cb852b19424
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-17 16:48:55 -0500

    Métodos de obtener todos los productos en el lado del cliente por medio del envio del formulario

--- ECommerceWeb/src/app/customer/components/dashboard-customer/dashboard-customer.component.ts
+++ ECommerceWeb/src/app/customer/components/dashboard-customer/dashboard-customer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { AdminService } from 'src/app/admin/service/admin.service';
+import { CustomerService } from '../../services/customer.service';
 
 @Component({
   selector: 'app-dashboard-customer',
@@ -7,4 +11,40 @@ import { Component } from '@angular/core';
 })
 export class DashboardCustomerComponent {
 
+  products: any[] = [];
+  searchProductForm!: FormGroup;
+
+
+  constructor ( private customerService: CustomerService, private fb: FormBuilder, private snackBar: MatSnackBar ) { }
+
+  ngOnInit() {
+    this.getAllProducts();
+    this.searchProductForm = this.fb.group({
+      title: [null, [Validators.required]]
+    })
+  }
+
+  getAllProducts() {
+    this.products = [];
+    this.customerService.getAllProducts().subscribe(res => {
+      // console.log('getAllProducts response:', res);
+      res.forEach(element => {
+        element.processedImg = 'data:image/png;base64,'+ element.byteImg;
+        this.products.push(element);
+      });
+    })
+  }
+
+  // Envio del formulario
+  submitForm() {
+    this.products = [];
+    const title = this.searchProductForm.get('title')!.value;
+    this.customerService.getAllProductsByName(title).subscribe(res => {
+      // console.log('getAllProducts response:', res);
+      res.forEach(element => {
+        element.processedImg = 'data:image/png;base64,'+ element.byteImg;
+        this.products.push(element);
+      });
+    })
+  }
 }
