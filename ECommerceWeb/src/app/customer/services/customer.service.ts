12481168fcf86a9769e2c87cf04715120114ad54
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-17 16:50:01 -0500

    Hay dos servicios que son: - Obtener todos los productos. - Obtener productos por el nombre.

---
+++ ECommerceWeb/src/app/customer/services/customer.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
+
+const BASIC_URL = "http://localhost:8080/" ;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CustomerService {
+
+  constructor( private http: HttpClient ) { }
+
+  getAllProducts(): Observable<any>{
+    return this.http.get(BASIC_URL + 'api/customer/products',{
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
+  getAllProductsByName(name: any): Observable<any>{
+    return this.http.get(BASIC_URL + `api/customer/search/${name}`,{
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
+  //  Método para autorizar
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set(
+      'Authorization', 'Bearer ' + UserStorageService.getToken()
+    )
+  }
+
+}
