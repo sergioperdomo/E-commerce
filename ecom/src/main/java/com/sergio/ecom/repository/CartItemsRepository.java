fe80d0617964e1467435abdfb8460c8dff3d95cd
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-18 09:56:17 -0500

    Método para encontrar el producto y el usuario por su id.

--- ecom/src/main/java/com/sergio/ecom/repository/CartItemsRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/CartItemsRepository.java
@@ -1,2 +1,13 @@
-package com.sergio.ecom.repository;public interface CartItemsRepository {
+package com.sergio.ecom.repository;
+
+import com.sergio.ecom.entity.CartItems;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CartItemsRepository extends JpaRepository<CartItems,Long> {
+
+    Optional<CartItems> findByProductIdAndUserId(Long productId, Long orderId, Long userId);
 }
