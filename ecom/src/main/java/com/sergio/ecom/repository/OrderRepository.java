f39a7e59689f92220d9f9b7d04dd93aca80337b6
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-18 10:00:37 -0500

    Método para encontrar el usuario por medio de su id y su estatus.

--- ecom/src/main/java/com/sergio/ecom/repository/OrderRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.sergio.ecom.repository;
 
 import com.sergio.ecom.entity.Order;
+import com.sergio.ecom.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Order findByUserIdAndStatus(Long userId, OrderStatus orderStatus);
 }
