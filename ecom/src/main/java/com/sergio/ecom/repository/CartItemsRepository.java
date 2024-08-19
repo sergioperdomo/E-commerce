a3ffffb918b83bf332283c572ac86b006796c433
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-19 12:18:50 -0500

    Eliminando un parametro que no se estaba utilizando y me estaba mostrando error a la hora de ejecutarlo.

--- ecom/src/main/java/com/sergio/ecom/repository/CartItemsRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/CartItemsRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CartItemsRepository extends JpaRepository<CartItems,Long> {
+public interface CartItemsRepository extends JpaRepository<CartItems, Long> {
 
-    Optional<CartItems> findByProductIdAndUserId(Long productId, Long orderId, Long userId);
+    Optional<CartItems> findByProductIdAndUserId(Long productId, Long userId);
 }
