4400d2126c54e1793ea1e805dfe403f78e793acb
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-19 20:54:47 -0500

    Añadiendo la ruta de nuestro nuevo componente cart en las rutas del cliente.

--- ECommerceWeb/src/app/customer/customer-routing.module.ts
+++ ECommerceWeb/src/app/customer/customer-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 // import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CustomerComponent } from './customer.component';
 import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
+import { CartComponent } from '../admin/components/cart/cart.component';
 
 
 const routes: Routes = [
   { path: '', component: CustomerComponent },
-  { path: 'dashboard', component: DashboardCustomerComponent }
+  { path: 'dashboard', component: DashboardCustomerComponent },
+  { path: 'cart', component: CartComponent }
 ];
 
 @NgModule({
