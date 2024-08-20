98d72463941d6fbf31b3913f52d41496b9afa279
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-19 21:15:36 -0500

    Método que nos permite obtener el carrito, en otras palabras nos permite llamar a esta API.

--- ECommerceWeb/src/app/admin/components/cart/cart.component.ts
+++ ECommerceWeb/src/app/admin/components/cart/cart.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from 'src/app/customer/services/customer.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+
+  carItems: any[] = [];
+  order: any;
+
+  constructor(
+    private customerService: CustomerService,
+    private snackbar: MatSnackBar,
+    private fb: FormBuilder,
+    public dialog: MatDialog
+  ) {}
+
+  ngOnInit(): void {
+    this.getCart();
+  }
+
+  // Método el cual permite llamar a nuestra API
+  getCart(){
+    this.carItems = [];
+    this.customerService.getCartByUserId().subscribe(res =>{
+      this.order = res;
+      res.cartItems.forEach(element => {
+        element.processedImg = 'data:image/png;base64,'+ element.returnedImg;
+        this.carItems.push(element);
+      })
+    })
+  }
 
 }
