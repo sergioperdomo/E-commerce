b1d80ee83aab80a10b45dc675f3911700fe6cdc4
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-19 20:37:03 -0500

    Método para obtener el carrito por medio del ID del usuario.

--- ecom/src/main/java/com/sergio/ecom/services/customer/cart/CartService.java
+++ ecom/src/main/java/com/sergio/ecom/services/customer/cart/CartService.java
@@ -1,8 +1,10 @@
 package com.sergio.ecom.services.customer.cart;
 
 import com.sergio.ecom.dto.AddProductInCartDto;
+import com.sergio.ecom.dto.OrderDto;
 import org.springframework.http.ResponseEntity;
 
 public interface CartService {
     ResponseEntity<?> addProductToCart(AddProductInCartDto addProductInCartDto);
+    OrderDto getCartByUserId(Long userId);
 }
