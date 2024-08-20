c095474ec3409b9a42a8365b4a148daf17197f42
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-20 17:13:04 -0500

    Creación del Coupon y CouponDto

---
+++ ecom/src/main/java/com/sergio/ecom/dto/CouponDto.java
@@ -0,0 +1,17 @@
+package com.sergio.ecom.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class CouponDto {
+
+    private Long id;
+
+    private String name;
+    private String code;
+    private Long discount;
+    private Date expirationDate;
+
+}
