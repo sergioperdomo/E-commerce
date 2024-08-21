6c1a447d2515e5789f9abe6d54bf4c8eb1d72e6d
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-20 21:49:00 -0500

    Agregando y obteniendo los cupones.

--- ECommerceWeb/src/app/admin/service/admin.service.ts
+++ ECommerceWeb/src/app/admin/service/admin.service.ts
@@ -52,6 +52,18 @@ export class AdminService {
     })
   }
 
+  addCoupon(couponDto: any): Observable<any>{
+    return this.http.post(BASIC_URL + 'api/admin/coupons', couponDto, {
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
+  getCoupons(): Observable<any>{
+    return this.http.get(BASIC_URL + 'api/admin/coupons', {
+      headers: this.createAuthorizationHeader(),
+    })
+  }
+
   //  Método para autorizar
   private createAuthorizationHeader(): HttpHeaders {
     return new HttpHeaders().set(
