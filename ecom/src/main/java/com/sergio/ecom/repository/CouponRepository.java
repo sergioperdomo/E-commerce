1bcdf5fed6af188667342cf9092f8c54ac8f5829
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-21 22:12:09 -0500

    El método busca un cupón con un código específico.

--- ecom/src/main/java/com/sergio/ecom/repository/CouponRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/CouponRepository.java
@@ -4,9 +4,13 @@ import com.sergio.ecom.entity.Coupon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon, Long> {
 
     // Método para verificar si el cupón existe.
     boolean existsByCode(String code);
+
+    Optional<Coupon> findByCode(String code);
 }
