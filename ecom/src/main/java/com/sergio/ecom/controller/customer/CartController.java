28529a7aa27ffaa0e479a3886846a4406d33460a
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-21 22:07:09 -0500

    Este método en particular trata de aplicar un cupón al carrito de compras de un usuario. Si tiene éxito, devuelve detalles actualizados de la orden. Si hay un error de validación (por ejemplo, el cupón no es válido), devuelve un mensaje de error con un estado de solicitud incorrecta.

--- ecom/src/main/java/com/sergio/ecom/controller/customer/CartController.java
+++ ecom/src/main/java/com/sergio/ecom/controller/customer/CartController.java
@@ -2,6 +2,7 @@ package com.sergio.ecom.controller.customer;
 
 import com.sergio.ecom.dto.AddProductInCartDto;
 import com.sergio.ecom.dto.OrderDto;
+import com.sergio.ecom.exceptions.ValidationException;
 import com.sergio.ecom.services.customer.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +27,16 @@ public class CartController {
     public ResponseEntity<?> addProductToCart(@PathVariable Long userId) {
         OrderDto orderDto = cartService.getCartByUserId(userId);
         return ResponseEntity.status(HttpStatus.OK).body(orderDto);
+    }
+
+    @GetMapping("/coupon/{userId}/{code}")
+    public ResponseEntity<?> applyCoupon(@PathVariable Long userId, @PathVariable String code){
+        try{
+            OrderDto orderDto = cartService.applyCoupon(userId, code);
+            return ResponseEntity.ok(orderDto);
+        } catch (ValidationException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
     }
 
 }
