420e798be63de361f3df6d03892bf2fb925dd472
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-22 10:58:51 -0500

    este método proporciona funcionalidad para aumentar la cantidad de un producto específico en el pedido pendiente de un usuario, manejar artículos del carrito existentes, actualizaciones del monto del pedido y posible aplicación de cupones. Devuelve los detalles actualizados del pedido en caso de éxito o nulo en caso de error.

--- ecom/src/main/java/com/sergio/ecom/services/customer/cart/CartService.java
+++ ecom/src/main/java/com/sergio/ecom/services/customer/cart/CartService.java
@@ -8,4 +8,5 @@ public interface CartService {
     ResponseEntity<?> addProductToCart(AddProductInCartDto addProductInCartDto);
     OrderDto getCartByUserId(Long userId);
     OrderDto applyCoupon(Long userId, String code);
+    OrderDto increaseProductQuantity(AddProductInCartDto addProductInCartDto);
 }
