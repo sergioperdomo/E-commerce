aaddd5e260b395f382deed45614b8a85bfc3c147
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-21 21:24:57 -0500

    Método para obtener los cupones.

--- ECommerceWeb/src/app/admin/components/coupons/coupons.component.ts
+++ ECommerceWeb/src/app/admin/components/coupons/coupons.component.ts
@@ -1,3 +1,5 @@
+import { AdminComponent } from './../../admin.component';
+import { AdminService } from './../../service/admin.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./coupons.component.scss']
 })
 export class CouponsComponent {
+
+  coupons: any;
+
+  constructor( private adminService: AdminService ){}
+
+  ngOnInit(){
+    this.getCoupons();
+  }
+
+  // Llamando la API get all coupon
+  getCoupons(){
+    this.adminService.getCoupons().subscribe(res => {
+      this.coupons = res;
+    })
+  }
 
 }
