2e7affe3613cbd16b32b512d8996eff381663ae4
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-22 22:21:21 -0500

    Creación del método de realizar el pedido.

--- ECommerceWeb/src/app/customer/components/place-order/place-order.component.ts
+++ ECommerceWeb/src/app/customer/components/place-order/place-order.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { CustomerService } from '../../services/customer.service';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-place-order',
@@ -7,4 +12,37 @@ import { Component } from '@angular/core';
 })
 export class PlaceOrderComponent {
 
+  orderForm!:FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private customerService: CustomerService,
+     private snackBar: MatSnackBar,
+     private router: Router,
+     private dialog: MatDialog
+  ){}
+
+  ngOnInit(){
+    // Mencionamos los controladores
+    this.orderForm = this.fb.group({
+      address: [null, [Validators.required]],
+      ordeDescription: [null],
+    })
+  }
+
+  placeOrder(){
+    this.customerService.placeOrder(this.orderForm.value).subscribe(res => {
+      if(res.id != null){
+        this.snackBar.open("Order placed successfully", "Close", { duration:3000 })
+        this.router.navigateByUrl("/customer/my-orders");
+        this.closeForm();
+      } else {
+        this.snackBar.open("Something went wrong", "Close", { duration: 3000 } )
+      }
+    })
+  }
+
+  closeForm(){
+    this.dialog.closeAll();
+  }
 }
