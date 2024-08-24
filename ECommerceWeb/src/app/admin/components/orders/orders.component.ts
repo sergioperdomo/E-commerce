c31aafdaf9775b2cef563ec85d56f363237d324d
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-24 11:15:23 -0500

    Verificamos que este llegando el token y si esta llegando correctamente. Se descarta que el error sea producto del token.

--- ECommerceWeb/src/app/admin/components/orders/orders.component.ts
+++ ECommerceWeb/src/app/admin/components/orders/orders.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AdminService } from '../../service/admin.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
 
 @Component({
   selector: 'app-orders',
@@ -20,8 +21,10 @@ export class OrdersComponent {
   }
 
   getPlacedOrders(){
+    console.log('Token: ', UserStorageService.getToken()); // En la consola del naevagor se puede observar que si llega el token.
     this.adminService.getPlacedOrders().subscribe(res => {
       this.orders = res;
+      console.log('Orders received:', res);
     })
   }
 
