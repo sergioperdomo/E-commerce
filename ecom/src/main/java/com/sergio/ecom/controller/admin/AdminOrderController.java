2d0a8c6c5fa29ccd90ac8a95a73ef4323c647fd7
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-25 12:01:40 -0500

    Este código define un punto final de API REST que maneja solicitudes HTTP GET para cambiar el estado de un pedido. Extrae el ID y el estado del pedido de la URL, llama a un servicio para cambiar el estado y devuelve una respuesta que indica el éxito o el fracaso de la operación.

--- ecom/src/main/java/com/sergio/ecom/controller/admin/AdminOrderController.java
+++ ecom/src/main/java/com/sergio/ecom/controller/admin/AdminOrderController.java
@@ -4,8 +4,10 @@ package com.sergio.ecom.controller.admin;
 import com.sergio.ecom.dto.OrderDto;
 import com.sergio.ecom.services.admin.adminOrder.AdminOrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +25,13 @@ public class AdminOrderController {
         return ResponseEntity.ok(adminOrderService.getAllPlacedOrders());
     }
 
+    @GetMapping("/order/{orderId}/{status}")
+    public ResponseEntity<?> changeOrderStatus(@PathVariable Long orderId, @PathVariable String status){
+        OrderDto orderDto = adminOrderService.changeOrderStatus(orderId, status);
+
+        if (orderDto == null){
+            return new ResponseEntity<>("Something went wrong!", HttpStatus.BAD_REQUEST);
+        }
+        return  ResponseEntity.status(HttpStatus.OK).body(orderDto);
+    }
 }
