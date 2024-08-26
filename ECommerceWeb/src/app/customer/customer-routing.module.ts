4c919bde5ce6ac734fd107f810688491d56c372e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-26 10:30:58 -0500

    Asignandole una ruta a nuestro componente.

--- ECommerceWeb/src/app/customer/customer-routing.module.ts
+++ ECommerceWeb/src/app/customer/customer-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerComponent } from './customer.component';
 import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
 import { CartComponent } from '../admin/components/cart/cart.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 
 
 const routes: Routes = [
   { path: '', component: CustomerComponent },
   { path: 'dashboard', component: DashboardCustomerComponent },
-  { path: 'cart', component: CartComponent }
+  { path: 'cart', component: CartComponent },
+  { path: 'my_orders', component: MyOrdersComponent}
 ];
 
 @NgModule({
