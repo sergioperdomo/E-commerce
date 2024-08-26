e87ca3b2e456e914bc8f9ccba6cd628723c0e362
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-26 10:28:38 -0500

    Se declara de forma automatica y se importa.

--- ECommerceWeb/src/app/customer/customer.module.ts
+++ ECommerceWeb/src/app/customer/customer.module.ts
@@ -7,13 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { DemoAngularMaterialModuleTsComponent } from '../DemoAngularMaterialModule';
 import { PlaceOrderComponent } from './components/place-order/place-order.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 
 
 @NgModule({
   declarations: [
     CustomerComponent,
     DashboardCustomerComponent,
-    PlaceOrderComponent
+    PlaceOrderComponent,
+    MyOrdersComponent
   ],
   imports: [
     CommonModule,
