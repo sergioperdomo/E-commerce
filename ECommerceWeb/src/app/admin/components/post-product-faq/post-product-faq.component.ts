2b5d194bd4422cd36856332a4ff2db5531b0bb8c
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-29 10:49:46 -0500

    Creación del componente

---
+++ ECommerceWeb/src/app/admin/components/post-product-faq/post-product-faq.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-product-faq',
+  templateUrl: './post-product-faq.component.html',
+  styleUrls: ['./post-product-faq.component.scss']
+})
+export class PostProductFaqComponent {
+
+}
