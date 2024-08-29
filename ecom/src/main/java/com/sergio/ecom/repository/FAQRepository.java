1348e3ebbd9e678d50ad9c0f846e291e41388527
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-29 10:35:22 -0500

    En resumen, esta interfaz FAQRepository te permite interactuar con entidades FAQ almacenadas en una base de datos utilizando métodos CRUD proporcionados por Spring Data JPA. No implementa la lógica real, pero te da acceso a funcionalidades como guardar, buscar, actualizar y eliminar FAQ de la base de datos.

---
+++ ecom/src/main/java/com/sergio/ecom/repository/FAQRepository.java
@@ -0,0 +1,12 @@
+package com.sergio.ecom.repository;
+
+import com.sergio.ecom.entity.FAQ;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FAQRepository extends JpaRepository<FAQ, Long> {
+
+
+
+
+
+}
