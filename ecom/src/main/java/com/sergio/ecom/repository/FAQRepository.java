ee5063cab0b939753fcd0b690c17f959762f6da5
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-08-30 10:59:08 -0500

    Añadiendo la anotación Repository

--- ecom/src/main/java/com/sergio/ecom/repository/FAQRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/FAQRepository.java
@@ -2,7 +2,9 @@ package com.sergio.ecom.repository;
 
 import com.sergio.ecom.entity.FAQ;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
 
 
