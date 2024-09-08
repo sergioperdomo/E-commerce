0c815e848c24ef0d4e97a5bb7463c46036ba7f34
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-08 12:49:40 -0500

    Añadio automaticamente.

--- ECommerceWeb/src/app/admin/admin.module.ts
+++ ECommerceWeb/src/app/admin/admin.module.ts
@@ -14,6 +14,7 @@ import { PostCouponComponent } from './components/post-coupon/post-coupon.compon
 import { CouponsComponent } from './components/coupons/coupons.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { PostProductFaqComponent } from './components/post-product-faq/post-product-faq.component';
+import { UpdateProductComponent } from './components/update-product/update-product.component';
 
 
 @NgModule({
@@ -27,6 +28,7 @@ import { PostProductFaqComponent } from './components/post-product-faq/post-prod
     CouponsComponent,
     OrdersComponent,
     PostProductFaqComponent,
+    UpdateProductComponent,
     // DashboardComponent
   ],
   imports: [
