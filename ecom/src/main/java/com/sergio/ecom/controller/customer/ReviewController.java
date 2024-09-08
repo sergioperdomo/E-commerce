69f5d3f25035e78e844ebdeae2517802e8171e19
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-08 17:48:29 -0500

    Este método controlador es responsable de gestionar solicitudes para obtener detalles de productos ordenados basados en un ID de orden. Extrae el ID de orden de la ruta de la URL, llama a un método de servicio para obtener los detalles de los productos y devuelve una entidad de respuesta que contiene la información recuperada.

---
+++ ecom/src/main/java/com/sergio/ecom/controller/customer/ReviewController.java
@@ -0,0 +1,26 @@
+package com.sergio.ecom.controller.customer;
+
+import com.sergio.ecom.dto.OrderedProductsResponseDto;
+import com.sergio.ecom.services.customer.review.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+
+    @GetMapping("/ordered-products/{orderId}")
+    public ResponseEntity<OrderedProductsResponseDto> getOrderedProductsDetailsByOrderId(@PathVariable Long orderId){
+        return ResponseEntity.ok(reviewService.getOrderedProductsDetailsByOrderId(orderId));
+    }
+
+
+}
