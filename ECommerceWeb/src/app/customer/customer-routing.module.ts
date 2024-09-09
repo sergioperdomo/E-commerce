d67cb5c5140a48c29320282ac31b3e39b31f1f29
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-09 11:59:58 -0500

    Dandole rutas a los dos componentes que creamos previamente

--- ECommerceWeb/src/app/customer/customer-routing.module.ts
+++ ECommerceWeb/src/app/customer/customer-routing.module.ts
@@ -1,3 +1,4 @@
+import { ViewOrderedProductsComponent } from './components/view-ordered-products/view-ordered-products.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { DashboardComponent } from './components/dashboard/dashboard.component';
@@ -5,13 +6,16 @@ import { CustomerComponent } from './customer.component';
 import { DashboardCustomerComponent } from './components/dashboard-customer/dashboard-customer.component';
 import { CartComponent } from '../admin/components/cart/cart.component';
 import { MyOrdersComponent } from './components/my-orders/my-orders.component';
+import { ReviewOrderedProductComponent } from './components/review-ordered-product/review-ordered-product.component';
 
 
 const routes: Routes = [
   { path: '', component: CustomerComponent },
   { path: 'dashboard', component: DashboardCustomerComponent },
   { path: 'cart', component: CartComponent },
-  { path: 'my_orders', component: MyOrdersComponent}
+  { path: 'my_orders', component: MyOrdersComponent},
+  { path: 'ordered_products/:orderId', component: ViewOrderedProductsComponent},
+  { path: 'review/:productId', component: ReviewOrderedProductComponent},
 ];
 
 @NgModule({
