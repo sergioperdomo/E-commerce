033e858cc31bd49ec5d2b50ec8707ddc10d69fed
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-11 15:45:27 -0500

    Cambios

---
+++ ecom/src/main/java/com/sergio/ecom/dto/ReviewDto.java
@@ -0,0 +1,20 @@
+package com.sergio.ecom.dto;
+
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+
+@Data
+public class ReviewDto {
+
+    private Long id;
+    private Long rating;
+    private String description;
+    private MultipartFile img;
+    private byte[] returnedImg;
+    private Long userId;
+    private Long productId;
+    private String username;
+
+
+}
