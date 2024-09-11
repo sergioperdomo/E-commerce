399de69322bcbf344ba70c35a99695e270ce0099
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-11 15:37:48 -0500

    Este componente permite que un usuario suba una reseña de un producto que ha comprado, incluyendo una calificación, una descripción y una imagen opcional del producto.

--- ECommerceWeb/src/app/customer/components/review-ordered-product/review-ordered-product.component.ts
+++ ECommerceWeb/src/app/customer/components/review-ordered-product/review-ordered-product.component.ts
@@ -1,4 +1,9 @@
+import { CustomerService } from './../../services/customer.service';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserStorageService } from 'src/app/services/storage/user-storage.service';
 
 @Component({
   selector: 'app-review-ordered-product',
@@ -6,5 +11,60 @@ import { Component } from '@angular/core';
   styleUrls: ['./review-ordered-product.component.scss']
 })
 export class ReviewOrderedProductComponent {
+
+  productId: number = this.activatedRoute.snapshot.params["productId"];
+  reviewForm!: FormGroup;
+  selectedFile: File | null;
+  imagePreview: String | ArrayBuffer | null;
+
+  constructor(
+    private fb: FormBuilder,
+    private snackBar: MatSnackBar,
+    private customerService: CustomerService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(){
+    this.reviewForm = this.fb.group({
+      rating: [null, [Validators.required]],
+      description: [null, [Validators.required]],
+    })
+  }
+
+  onFileSelected(event: any){
+    this.selectedFile = event.target.files[0];
+    this.previewImage();
+  }
+
+  previewImage(){
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result;
+    }
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  submitForm(){
+    const formData: FormData = new FormData();
+    formData.append('img', this.selectedFile)
+    formData.append('productId', this.productId.toString());
+    formData.append('userId', UserStorageService.getUserId().toString());
+    formData.append('rating', this.reviewForm.get('rating').value);
+
+    this.customerService.giveReview(formData).subscribe(res => {
+      if(res.id != null){
+        this.snackBar.open('Review Posted Successfully!', 'Close', {
+          duration: 3000
+        });
+        this.router.navigateByUrl('/customer/my_orders');
+      } else {
+        this.snackBar.open('Something went wrong', 'Error', {
+          duration: 3000
+        });
+      }
+    })
+  }
+
 
 }
