44df2ec5df75a3936f66d90a5a994059de6572bb
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-12 12:06:10 -0500

    Creación del componente

---
+++ ECommerceWeb/src/app/customer/components/view-product-detail/view-product-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-product-detail',
+  templateUrl: './view-product-detail.component.html',
+  styleUrls: ['./view-product-detail.component.scss']
+})
+export class ViewProductDetailComponent {
+
+}
