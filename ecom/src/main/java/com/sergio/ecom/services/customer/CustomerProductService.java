45ab17fe3eaa6afd22fe880213a41f5f8315f6b2
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-12 11:45:40 -0500

    Este método permite devolver una vista detallada del producto, sus preguntas frecuentes y reseñas en un solo objeto ProductDetailDto.

--- ecom/src/main/java/com/sergio/ecom/services/customer/CustomerProductService.java
+++ ecom/src/main/java/com/sergio/ecom/services/customer/CustomerProductService.java
@@ -1,5 +1,6 @@
 package com.sergio.ecom.services.customer;
 
+import com.sergio.ecom.dto.ProductDetailDto;
 import com.sergio.ecom.dto.ProductDto;
 
 import java.util.List;
@@ -7,6 +8,6 @@ import java.util.List;
 public interface CustomerProductService {
 
     List<ProductDto> searchProductByTitle(String title);
-
     List<ProductDto> getAllProducts();
+    ProductDetailDto getProductDetailById(Long productId);
 }
--- ecom/src/main/java/com/sergio/ecom/services/customer/CustomerProductServiceImpl.java
+++ ecom/src/main/java/com/sergio/ecom/services/customer/CustomerProductServiceImpl.java
@@ -1,22 +1,30 @@
 package com.sergio.ecom.services.customer;
 
 
+import com.sergio.ecom.dto.ProductDetailDto;
 import com.sergio.ecom.dto.ProductDto;
+import com.sergio.ecom.entity.FAQ;
 import com.sergio.ecom.entity.Product;
+import com.sergio.ecom.entity.Review;
+import com.sergio.ecom.repository.FAQRepository;
 import com.sergio.ecom.repository.ProductRepository;
+import com.sergio.ecom.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerProductServiceImpl implements CustomerProductService {
 
-    @Autowired
-    public ProductRepository productRepository;
+    private final ProductRepository productRepository;
+    private final FAQRepository faqRepository;
+    private final ReviewRepository reviewRepository;
+
 
     public List<ProductDto> getAllProducts() {
         List<Product> products = productRepository.findAll();
@@ -27,4 +35,22 @@ public class CustomerProductServiceImpl implements CustomerProductService {
         List<Product> products = productRepository.findAllByNameContaining(name);
         return products.stream().map(Product::getDto).collect(Collectors.toList());
     }
+
+    public ProductDetailDto getProductDetailById(Long productId){
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (optionalProduct.isPresent()){
+            List<FAQ> faqList = faqRepository.findAllByProductId(productId);
+            List<Review> reviewList = reviewRepository.findAllByProductId(productId);
+
+            ProductDetailDto productDetailDto = new ProductDetailDto();
+
+            productDetailDto.setProductDto(optionalProduct.get().getDto());
+            productDetailDto.setFaqDtoList(faqList.stream().map(FAQ::getFAQDto).collect(Collectors.toList()));
+            productDetailDto.setReviewDtoList(reviewList.stream().map(Review::getDto).collect(Collectors.toList()));
+
+            return productDetailDto;
+        }
+        return null;
+    }
+
 }
