00609b11ab8c7407da13df1257a55340b5861494
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-12 11:46:54 -0500

    Este repositorio permite buscar todas las reseñas que estén asociadas a un producto específico mediante el método findAllByProductId

--- ecom/src/main/java/com/sergio/ecom/repository/ReviewRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/ReviewRepository.java
@@ -1,10 +1,14 @@
 package com.sergio.ecom.repository;
 
 
+import com.sergio.ecom.entity.FAQ;
 import com.sergio.ecom.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findAllByProductId(Long productsId);
 }
