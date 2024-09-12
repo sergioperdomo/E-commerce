6dcfb4ddd322c07700e5578d96494bad481c6cad
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-12 11:50:43 -0500

    Este repositorio permite realizar las operaciones CRUD estándar sobre la entidad FAQ y agrega un método específico para obtener todas las FAQs relacionadas con un producto en particular mediante su productId.

--- ecom/src/main/java/com/sergio/ecom/repository/FAQRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/FAQRepository.java
@@ -4,10 +4,12 @@ import com.sergio.ecom.entity.FAQ;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FAQRepository extends JpaRepository<FAQ, Long> {
 
-
+    List<FAQ> findAllByProductId(Long productsId);
 
 
 
