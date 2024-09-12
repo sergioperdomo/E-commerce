39b196233c9f3d9b59ed319b6053e9a785b5b365
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-12 11:47:44 -0500

    ProductDetailDto es una clase que contiene los datos necesarios para representar un producto y sus detalles relacionados, como las reseñas y preguntas frecuentes, facilitando el transporte de estos datos entre partes de la aplicación.

---
+++ ecom/src/main/java/com/sergio/ecom/dto/ProductDetailDto.java
@@ -0,0 +1,14 @@
+package com.sergio.ecom.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class ProductDetailDto {
+
+    private ProductDto productDto;
+    private List<ReviewDto> reviewDtoList;
+    private List<FAQDto> faqDtoList;
+
+}
