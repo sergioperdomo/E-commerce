aba21fa19c83f65c898ca1b92fe3e95bf2552098
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-14 18:05:27 -0500

    Básicamente lo que se hace es obtener la información de un producto en especifico por medio de su ID y mostrarla.

--- ECommerceWeb/src/app/customer/components/view-product-detail/view-product-detail.component.ts
+++ ECommerceWeb/src/app/customer/components/view-product-detail/view-product-detail.component.ts
@@ -1,4 +1,7 @@
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Component } from '@angular/core';
+import { CustomerService } from '../../services/customer.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-view-product-detail',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-product-detail.component.scss']
 })
 export class ViewProductDetailComponent {
+
+  productId: number = this.activatedRoute.snapshot.params["productId"];
+  product: any;
+  FAQS: any[] = [];
+  reviews: any[] = [];
+
+  constructor(
+    private snackBar: MatSnackBar,
+    private customerService: CustomerService,
+    private activatedRoute: ActivatedRoute
+  ){}
+
+  ngOnInit(){
+    this.getProductDetailById();
+  }
+
+  getProductDetailById(){
+    this.customerService.getProductDetailById(this.productId).subscribe(res => {
+      this.product = res.productDto;
+      this.product.processedImg = 'data:image/png;base64,' + res.productDto.byteImg;
+
+      this.FAQS = res.faqDtoList;
+
+      res.reviewDtoList.forEach(element => {
+        element.processedImg = 'data:image/png;base64,' + element.returnedImg;
+        this.reviews.push(element);
+      })
+    })
+  }
 
 }
