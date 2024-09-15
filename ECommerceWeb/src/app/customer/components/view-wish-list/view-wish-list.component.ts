27e400cc27e1cd5feb2bcf5aeda34e5caeddb5db
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-15 13:35:12 -0500

    Creación del componente.

---
+++ ECommerceWeb/src/app/customer/components/view-wish-list/view-wish-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-wish-list',
+  templateUrl: './view-wish-list.component.html',
+  styleUrls: ['./view-wish-list.component.scss']
+})
+export class ViewWishListComponent {
+
+}
