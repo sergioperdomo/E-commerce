e9f6fff6408912efe5499991ec00f6218e4b67f8
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-15 13:31:30 -0500

    Busca todas las listas de deseos asociadas a un usuario específico según su ID.

--- ecom/src/main/java/com/sergio/ecom/repository/WishListRepository.java
+++ ecom/src/main/java/com/sergio/ecom/repository/WishListRepository.java
@@ -4,7 +4,11 @@ import com.sergio.ecom.entity.WishList;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface WishListRepository extends JpaRepository<WishList, Long> {
+
+    List<WishList> findAllByUserId(Long userId);
 
 }
