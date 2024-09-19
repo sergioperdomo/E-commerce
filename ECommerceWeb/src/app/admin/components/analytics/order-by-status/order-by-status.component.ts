4f8f1553b45260d4186e78eccc756b74ee0b7bf2
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-19 12:46:51 -0500

    Creación del componente

---
+++ ECommerceWeb/src/app/admin/components/analytics/order-by-status/order-by-status.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-order-by-status',
+  templateUrl: './order-by-status.component.html',
+  styleUrls: ['./order-by-status.component.scss']
+})
+export class OrderByStatusComponent {
+
+}
