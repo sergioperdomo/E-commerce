3f241a058ccc123afaf56cd2c9900bac73b38467
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-19 12:25:21 -0500

    Creación de los atributos que se van a utilizar previamente.

--- ecom/src/main/java/com/sergio/ecom/dto/AnalyticsResponse.java
+++ ecom/src/main/java/com/sergio/ecom/dto/AnalyticsResponse.java
@@ -1,2 +1,17 @@
-package com.sergio.ecom.dto;public class AnalyticsResponse {
+package com.sergio.ecom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AnalyticsResponse {
+
+    private Long placed;
+    private Long shipped;
+    private Long delivered;
+    private Long currentMonthOrders;
+    private Long previousMonthOrders;
+    private Long currentMonthEarnings;
+    private Long previousMonthEarnings;
 }
