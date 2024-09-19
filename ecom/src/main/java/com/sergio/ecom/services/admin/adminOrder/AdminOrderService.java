2ab6982ce7011b47d060de8e6fb41db71e2de517
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-19 12:29:32 -0500

    Este método calcula y devuelve un informe con:
    El número de órdenes realizadas, enviadas y entregadas.
    El total de órdenes del mes actual y del mes anterior.
    Las ganancias del mes actual y del mes anterior.
    Estos datos son encapsulados en un objeto AnalyticsResponse, que luego puede ser utilizado para mostrar estadísticas en un dashboard o enviarse como respuesta en una API REST.

--- ecom/src/main/java/com/sergio/ecom/services/admin/adminOrder/AdminOrderService.java
+++ ecom/src/main/java/com/sergio/ecom/services/admin/adminOrder/AdminOrderService.java
@@ -1,5 +1,6 @@
 package com.sergio.ecom.services.admin.adminOrder;
 
+import com.sergio.ecom.dto.AnalyticsResponse;
 import com.sergio.ecom.dto.OrderDto;
 
 import java.util.List;
@@ -7,6 +8,9 @@ import java.util.List;
 public interface AdminOrderService {
 
     List<OrderDto> getAllPlacedOrders();
+
     OrderDto changeOrderStatus (Long orderId, String status);
+
+    AnalyticsResponse calculateAnalytics();
 
 }
