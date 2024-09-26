25baa233323ffba728727b19103adb3bc1bfa7d5
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-26 10:57:43 -0500

    Declarando e Importnado los dos componentes

--- ECommerceWeb/src/app/admin/admin.module.ts
+++ ECommerceWeb/src/app/admin/admin.module.ts
@@ -15,6 +15,8 @@ import { CouponsComponent } from './components/coupons/coupons.component';
 import { OrdersComponent } from './components/orders/orders.component';
 import { PostProductFaqComponent } from './components/post-product-faq/post-product-faq.component';
 import { UpdateProductComponent } from './components/update-product/update-product.component';
+import { AnalyticsComponent } from './components/analytics/analytics.component';
+import { OrderByStatusComponent } from './components/analytics/order-by-status/order-by-status.component';
 
 
 @NgModule({
@@ -29,6 +31,8 @@ import { UpdateProductComponent } from './components/update-product/update-produ
     OrdersComponent,
     PostProductFaqComponent,
     UpdateProductComponent,
+    AnalyticsComponent,
+    OrderByStatusComponent,
     // DashboardComponent
   ],
   imports: [
