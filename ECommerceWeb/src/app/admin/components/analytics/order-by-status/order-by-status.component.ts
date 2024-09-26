81e504b357913206caeeb69990fe57b2c6f6824e
Sergio Andres Perdomo Ortiz <perdomoortizsergio@gmail.com>
2024-09-26 11:00:59 -0500

    Este componente está preparado para recibir datos desde un componente padre mediante la propiedad data decorada con @Input().

--- ECommerceWeb/src/app/admin/components/analytics/order-by-status/order-by-status.component.ts
+++ ECommerceWeb/src/app/admin/components/analytics/order-by-status/order-by-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-order-by-status',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./order-by-status.component.scss']
 })
 export class OrderByStatusComponent {
+
+  @Input() data:any;
 
 }
